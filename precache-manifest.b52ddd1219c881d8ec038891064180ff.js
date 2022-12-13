9a9607583fccdd3a68e4bfba1eeafb1f431e196b
DrJuneMoone <96371033+MooneDrJune@users.noreply.github.com>
2022-12-13 15:55:37 +0530

    updated

---
+++ precache-manifest.b52ddd1219c881d8ec038891064180ff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7fbddb5235b51f91c42dba1c878da61",
+    "url": "/oc/index.html"
+  },
+  {
+    "revision": "afe2fec9bea0a0370d83",
+    "url": "/oc/static/css/2.d5bec64a.chunk.css"
+  },
+  {
+    "revision": "1d8eaf6968555fd81e35",
+    "url": "/oc/static/css/main.498d002a.chunk.css"
+  },
+  {
+    "revision": "afe2fec9bea0a0370d83",
+    "url": "/oc/static/js/2.8654536a.chunk.js"
+  },
+  {
+    "revision": "35f1a3ca561e93b64bcba705f76bd30f",
+    "url": "/oc/static/js/2.8654536a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d8eaf6968555fd81e35",
+    "url": "/oc/static/js/main.c85ab9fb.chunk.js"
+  },
+  {
+    "revision": "96097b7bcfd0fbcf9c01",
+    "url": "/oc/static/js/runtime-main.ba419ae8.js"
+  }
+]);
