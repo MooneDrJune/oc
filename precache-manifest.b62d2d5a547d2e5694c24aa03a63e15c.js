4482b252e02e30faaa1d67e5c9615c54e8114da8
DrJuneMoone <96371033+MooneDrJune@users.noreply.github.com>
2022-12-13 15:20:06 +0530

    Add files via upload

---
+++ precache-manifest.b62d2d5a547d2e5694c24aa03a63e15c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e433feb509a4e953f654a880e3decd77",
+    "url": "/index.html"
+  },
+  {
+    "revision": "afe2fec9bea0a0370d83",
+    "url": "/static/css/2.d5bec64a.chunk.css"
+  },
+  {
+    "revision": "67d17c983484138d5ac5",
+    "url": "/static/css/main.498d002a.chunk.css"
+  },
+  {
+    "revision": "afe2fec9bea0a0370d83",
+    "url": "/static/js/2.8654536a.chunk.js"
+  },
+  {
+    "revision": "35f1a3ca561e93b64bcba705f76bd30f",
+    "url": "/static/js/2.8654536a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67d17c983484138d5ac5",
+    "url": "/static/js/main.9899020b.chunk.js"
+  },
+  {
+    "revision": "fb4a7f2fbe400027545e",
+    "url": "/static/js/runtime-main.93bbd796.js"
+  }
+]);
